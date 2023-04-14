50f31d3ee906d142ec9d3ea9d9f15b16132c0224
Ebi Abaje <eabaje@outlook.com>
2023-04-14 20:01:49 +0100

    env.production update

--- constants/index.js
+++ constants/index.js
@@ -3,7 +3,7 @@ export const LOG_IN = "http://";
 
 /* local host environment variables */
 
-export const API_URL = "http://localhost:8000/api/";
+export const API_URL = process.env.API_ENDPOINT;
 
 export const MEDIA_URL = "http://localhost:8000/";
 
